f3c66c76f992c6fd04752e696489549a17a1b551
Sina <sinakhorami94@gmail.com>
2020-11-06 16:10:22 +0330

    feat: added instagram scraper adapter

--- rest/app/main/adapter/instagram.py
+++ rest/app/main/adapter/instagram.py
@@ -2,6 +2,7 @@ import hashlib
 import string
 import random
 from instagram_web_api import Client
+from igramscraper.instagram import Instagram
 
 
 class InstagramWebClient(Client):
@@ -42,4 +43,30 @@ class InstagramWebAdapter:
         items.extend(reversed(edges))
 
         return items, new_end_cursor, has_next_page
+
+
+class InstagramScraperAdapter:
+
+    def __init__(self):
+        self.api = Instagram()
+
+    @staticmethod
+    def parse_comment(comment):
+        return {
+            'id': comment.identifier,
+            'text': comment.text,
+            'created_at': comment.created_at,
+            'username': comment.owner.username
+        }
+
+    def get_media_comments(self, shortcode, end_cursor=None, count=200):
+        items = self.api.get_media_comments_by_code(code=shortcode, count=count, max_id=end_cursor)
+
+        comments = items['comments']
+        new_end_cursor = items['next_page']
+        has_next_page = True if new_end_cursor else False
+
+        comments = [self.parse_comment(cm) for cm in comments]
+
+        return comments, new_end_cursor, has_next_page
 
--- rest/app/main/service/instagram.py
+++ rest/app/main/service/instagram.py
@@ -1,10 +1,12 @@
 from app.main.adapter.instagram import InstagramWebAdapter
+from app.main.adapter.instagram import InstagramScraperAdapter
 
 
 class InstagramService:
 
     def __init__(self):
-        self.adapter = InstagramWebAdapter()
+        # self.adapter = InstagramWebAdapter()
+        self.adapter = InstagramScraperAdapter()
 
     def get_comments(self, shortcode, end_cursor=None):
         comments, new_end_cursor, has_next_page = self.adapter.get_media_comments(shortcode=shortcode, end_cursor=end_cursor)
