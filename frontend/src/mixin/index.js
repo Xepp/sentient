22f985eaaab01581e3d6967e1cfa3816bc858679
Sina <sinakhorami94@gmail.com>
2021-07-16 17:21:58 +0430

    Feat: Added filter section based on search & sentiment
    Fixes #28

--- frontend/src/mixin/index.js
+++ frontend/src/mixin/index.js
@@ -1,10 +1,12 @@
 import GeneralMixin from './general'
+import FilterMixin from './filter'
 import TelegramMixin from './telegram'
 import TwitterMixin from './twitter'
 import KhabarFooriMixin from './web'
 
 export {
   GeneralMixin,
+  FilterMixin,
   TelegramMixin,
   TwitterMixin,
   KhabarFooriMixin
