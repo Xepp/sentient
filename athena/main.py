62bc99d52264ab0cfa451a87b09478c9ac4208da
Sina <sinakhorami94@gmail.com>
2021-08-02 00:13:06 +0430

    Feat: Added new athena structure and models (#48)
    Fixes #41

--- athena/main.py
+++ athena/main.py
@@ -1,15 +1,29 @@
 import json
-
+import numpy as np
 from flask import Flask
 from flask import request
-
-from model import Model
+from dataset import get_dataset
+from preprocess import text_cleaner
+from model import LinearSVCModel
+from model import SparseModel
 
 
 app = Flask(__name__)
-model = Model()
 
-@app.route("/",  methods = ['POST'])
+X_train, Y_train, X_test, Y_test = get_dataset(['sina_v1.csv', 'sina_v2.csv'])
+X_train = X_train.map(text_cleaner)
+X_test = X_test.map(text_cleaner)
+X_train = np.asarray(X_train)
+Y_train = np.asarray(Y_train)
+X_test = np.asarray(X_test)
+Y_test = np.asarray(Y_test)
+
+#  model = LinearSVCModel()
+model = SparseModel()
+model.load_or_train(X_train, Y_train, X_test, Y_test)
+
+
+@app.route("/",  methods=['POST'])
 def index():
     data = request.get_json()
     predicted = model.predict(data['sentences'])
@@ -19,4 +33,3 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
-
